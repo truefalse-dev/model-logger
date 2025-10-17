46b04a10fbf87c1a424d334809ce8fb19836b9e7
Timur Logachov <timur.logachov@teqblaze.com>
2025-10-17 16:00:49 +0300

    Initial package commit

---
+++ ModelLoggerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Truefalse\ModelLogger;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModelLoggerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
---
+++ src/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!function_exists('modelLogger')) {
+    function modelLogger($param)
+    {
+        return strtoupper($param);
+    }
+}
