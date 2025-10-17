cf7046534e32678063779b9fbcae868f9868ba98
Timur Logachov <timur.logachov@teqblaze.com>
2025-10-17 16:37:07 +0300

    change structure 4

--- src/ModelLogger/ServiceProvider.php
+++ src/ModelLogger/ServiceProvider.php
@@ -7,14 +7,14 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/model-logger.php' => config_path('model-logger.php'),
+            __DIR__.'/config/model-logger.php' => config_path('model-logger.php'),
         ], 'config');
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/model-logger.php', 'model-logger'
+            __DIR__.'/config/model-logger.php', 'model-logger'
         );
     }
 }
